5a88c0b090fa5f8b8c1939d07cda04478e7ad5c8
Anuj pal <panuj8141@gmail.com>
2025-07-01 14:18:52 +0530

    solving reference error

--- login/login.js
+++ login/login.js
@@ -46,7 +46,7 @@ window.onload=checkForLogin();
     if(email && password){
         signInWithEmailAndPassword(auth , email , password).
             then((userCredentials)=>{
-                window.location.replace("../index.html");
+                window.location.replace("/finalV2");
                 
             }).
             catch((error)=>{                

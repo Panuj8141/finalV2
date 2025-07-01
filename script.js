3edbc81808fcd01188011595e80a7f79575a7cec
Anuj pal <panuj8141@gmail.com>
2025-07-01 10:57:59 +0530

    making changes to suit the github pages for pwa

--- script.js
+++ script.js
@@ -179,7 +179,7 @@ search.addEventListener("input", (e) => {
   })
 
   if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/service-worker.js')
+    navigator.serviceWorker.register('service-worker.js')
       .then(() => console.log('Service Worker Registered'))
       .catch((error) => console.error('Service Worker registration failed:', error));
   }
